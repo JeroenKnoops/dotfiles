4ef306e56fd9c4dda110d851d4211ba52b277f64
Jeroen Knoops <jeroen.knoops@philips.com>
2025-06-11 10:37:02 +0200

    some extra config
    Signed-off-by: Jeroen Knoops <jeroen.knoops@philips.com>

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,3 @@
--- Some keyboard mappings as I don't want to break my fingers, while typing on a "german" keyboard ;)
-vim.opt.langmap = "+]ü["
--- Plain langmap remapping does not seem to do the trick :(
-vim.keymap.set("n", "ü", "[", { remap = true })
-
 vim.opt.expandtab = true -- Convert tabs to spaces
 vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
 vim.opt.tabstop = 4 -- How many spaces are shown per Tab
@@ -31,8 +26,7 @@ vim.opt.mouse = "a"
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
